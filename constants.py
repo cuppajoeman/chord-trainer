42dc51a60ae1135c36e59ff4bbf71f0a9a4d70ba
kool koder <email@com.gmail>
2021-09-09 20:05:35 -0400

    add in guitar mode.

--- constants.py
+++ constants.py
@@ -1,8 +1,36 @@
-MAJOR_DIATONIC_QUALITIES = {
+BASE_QUALITIES = {
+    "maj": (0, 4, 7),
+    "min": (0, 3, 7),
+    "dim": (0, 3, 6),
+    "aug": (0, 4, 8),
+}
+
+# the major and minor scales have the same qualities
+MAJOR_MINOR_DIATONIC_QUALITIES = {
     "maj7": (0, 4, 7, 11),
     "min7": (0, 3, 7, 10),
     "dom7": (0, 4, 7, 10),
     "half-dim7": (0, 3, 6, 10),
+}
+
+
+OTHER_QUALITIES = {
+    "dim": (0, 3, 6, 9),
+    "minmaj7": (0, 3, 7, 11),
+    "aug7": (0, 4, 8, 10),
+    "maj6": (0, 4, 7, 9),
+    "min6": (0, 3, 7, 9),
+}
+
+
+QUALITY_ALTERATIONS = {
+    "b9": (1,),
+    "9": (2,),
+    "#9": (3,),
+    "#5": (8,),
+    "#11": (6,),
+    "sus4": (5,),
+    "sus2": (2,),
 }
 
 NOTE_NUMBER_TO_STANDARD_NAME = [
